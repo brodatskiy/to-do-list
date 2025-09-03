34b6997b8f61ab4536dc0dacf1b354d52bdc2d28
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-03 17:42:14 +0300

    Добавляет поддержку мягкого удаления в таблицу задач.

--- database/migrations/2025_09_01_171204_create_tasks_table.php
+++ database/migrations/2025_09_01_171204_create_tasks_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->enum('status', TaskStatus::values())->default(TaskStatus::NotStarted);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
